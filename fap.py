b8164a1282136035edcf4bf6d53782d926f1df45
null3yte <ebrahymym85@gmail.com>
2023-07-04 01:36:13 -0700

    Added thread for -f

--- fap.py
+++ fap.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import argparse
+import concurrent.futures
 import re
 import asyncio
 import aiohttp
 from lxml import html
 
 
-async def single(url: str) -> set:
+async def extractor(url: str) -> set:
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             content = await response.text()
@@ -25,44 +26,27 @@ async def single(url: str) -> set:
             return keywords
 
 
-async def mass(url: str) -> set:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            content = await response.text()
-            tree = html.fromstring(content)
-
-            keywords = set()
-            if response.status == 200:
-                for element in tree.xpath('//input[@name] | //select[@name]'):
-                    keywords.add(element.get('name'))
-                    keywords.add(element.get('id'))
-
-                for var in tree.xpath('//script[contains(text(), "var ")]'):
-                    vars_list = re.findall(r'var\s+(\w+)', var.text)
-                    keywords.update(vars_list)
-            return keywords
-
-
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', type=str, help='scheme://domain.tld/path')
     parser.add_argument('-f', '--file', type=str, help='URLs file path')
+    parser.add_argument('-t', '--thread', type=int, default=4, help='maximum number of threads to use (default: 4)')
 
     args = parser.parse_args()
     url = args.url
     urls = args.file
+    thread = args.thread
 
     keywords = set()
     if url:
-        keywords = await single(url)
+        keywords = await extractor(url)
     elif urls:
-        async with aiohttp.ClientSession() as session:
-            with open(urls, "r") as f:
-                tasks = []
-                for line in f:
-                    tasks.append(mass(line.strip()))
-                results = await asyncio.gather(*tasks)
-                for result in results:
+        with open(urls, "r") as f:
+            urls_list = [url.strip() for url in f.readlines()]
+            with concurrent.futures.ThreadPoolExecutor(thread) as executor:
+                tasks = [asyncio.create_task(extractor(url)) for url in urls_list]
+                for coro in asyncio.as_completed(tasks):
+                    result = await coro
                     keywords.update(result)
 
     for keyword in keywords:
