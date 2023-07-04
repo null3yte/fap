e3d0e6cc6990b4e4e3bfbb0c077ec2c080c08007
null3yte <ebrahymym85@gmail.com>
2023-07-04 00:43:25 -0700

    Optimization and speed increase

--- fap.py
+++ fap.py
@@ -1,53 +1,49 @@
 #!/usr/bin/env python
 import argparse
 import re
-import requests
-from bs4 import BeautifulSoup
-import sys
+import asyncio
+import aiohttp
+from lxml import html
 
 
-def single(url: str) -> set:
-    response = requests.get(url)
-    content = response.text
-    soup = BeautifulSoup(content, 'html.parser')
+async def single(url: str) -> set:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.text()
+            tree = html.fromstring(content)
 
-    keywords = set()
-    if response.status_code == 200:
-        for tag in soup.find_all(re.compile(r'.*'), {'id': True}):
-            keywords.add(tag['id'])
+            keywords = set()
+            if response.status == 200:
+                keywords.update(tree.xpath('//*[@id]/@id'))
+                keywords.update(tree.xpath('//*[@name]/@name'))
 
-        for tag in soup.find_all(re.compile(r'.*'), {'name': True}):
-            keywords.add(tag['name'])
-
-        for var in soup.find_all('script', string=re.compile(r'var\s+\w+')):
-            vars_list = re.findall(r'var\s+(\w+)', var.text)
-            keywords.update(vars_list)
-    else:
-        print(f"status code: {response.status_code}")
-    return keywords
+                for var in tree.xpath('//script[contains(text(), "var ")]'):
+                    vars_list = re.findall(r'var\s+(\w+)', var.text)
+                    keywords.update(vars_list)
+            else:
+                print(f"status code: {response.status}")
+            return keywords
 
 
-def mass(url: str) -> set:
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, "html.parser")
+async def mass(url: str) -> set:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            content = await response.text()
+            tree = html.fromstring(content)
 
-    keywords = set()
+            keywords = set()
+            if response.status == 200:
+                for element in tree.xpath('//input[@name] | //select[@name]'):
+                    keywords.add(element.get('name'))
+                    keywords.add(element.get('id'))
 
-    if response.status_code == 200:
-        for element in soup.find_all(['input', 'select']):
-            if element.get('name'):
-                keywords.add(element['name'])
-            if element.get('id'):
-                keywords.add(element['id'])
-
-        script_tags = soup.find_all('script', string=re.compile(r'var\s+\w+'))
-        for tag in script_tags:
-            variables = re.findall(r'var\s+(\w+)', tag.string)
-            keywords.update(variables)
-    return keywords
+                for var in tree.xpath('//script[contains(text(), "var ")]'):
+                    vars_list = re.findall(r'var\s+(\w+)', var.text)
+                    keywords.update(vars_list)
+            return keywords
 
 
-def main():
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', type=str, help='scheme://domain.tld/path')
     parser.add_argument('-f', '--file', type=str, help='URLs file path')
@@ -56,19 +52,22 @@ def main():
     url = args.url
     urls = args.file
 
+    keywords = set()
     if url:
-        for keyword in single(url):
-            print(keyword)
+        keywords = await single(url)
     elif urls:
-        keywords = set()
-        with open(urls, "r") as f:
-            for line in f:
-                url_keywords = mass(line.strip())
-                for keyword in url_keywords:
-                    keywords.add(keyword)
-        for keyword in keywords:
-            print(keyword)
+        async with aiohttp.ClientSession() as session:
+            with open(urls, "r") as f:
+                tasks = []
+                for line in f:
+                    tasks.append(mass(line.strip()))
+                results = await asyncio.gather(*tasks)
+                for result in results:
+                    keywords.update(result)
+
+    for keyword in keywords:
+        print(keyword)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
