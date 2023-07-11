9d0673e9e22c93f7e3ded2f66c90f45b97f7c635
null3yte <ebrahymym85@gmail.com>
2023-07-11 09:41:02 -0700

    Improve URL parameter parsing and optimize code

--- fap.py
+++ fap.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python
 import argparse
-import concurrent.futures
 import re
 import asyncio
-import aiohttp
+import httpx
 from lxml import html
+from urllib.parse import urlparse, parse_qs
 
 
 async def extractor(url: str) -> set:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            content = await response.text()
-            tree = html.fromstring(content)
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        content = response.text
+        tree = html.fromstring(content)
 
-            keywords = set()
-            if response.status == 200:
-                keywords.update(tree.xpath('//*[@id]/@id'))
-                keywords.update(tree.xpath('//*[@name]/@name'))
+        keywords = set()
+        if response.status_code == 200:
+            keywords.update(tree.xpath('//*[@id]/@id'))
+            keywords.update(tree.xpath('//*[@name]/@name'))
 
-                for var in tree.xpath('//script[contains(text(), "var ")]'):
-                    vars_list = re.findall(r'var\s+(\w+)', var.text)
-                    keywords.update(vars_list)
-            else:
-                print(f"status code: {response.status}")
-            return keywords
+            for var in tree.xpath('//script[contains(text(), "var ")]'):
+                vars_list = re.findall(r'var\s+(\w+)', var.text)
+                keywords.update(vars_list)
+
+            # Extract parameter names
+            parsed_url = urlparse(url)
+            query_params = parse_qs(parsed_url.query)
+            param_names = query_params.keys()
+            keywords.update(param_names)
+
+        else:
+            print(f"status code: {response.status_code}")
+
+        return keywords
 
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-u', '--url', type=str, help='scheme://domain.tld/path')
     parser.add_argument('-f', '--file', type=str, help='URLs file path')
-    parser.add_argument('-t', '--thread', type=int, default=4, help='maximum number of threads to use (default: 4)')
 
     args = parser.parse_args()
     url = args.url
     urls = args.file
-    thread = args.thread
 
     keywords = set()
     if url:
@@ -43,11 +49,10 @@ async def main():
     elif urls:
         with open(urls, "r") as f:
             urls_list = [url.strip() for url in f.readlines()]
-            with concurrent.futures.ThreadPoolExecutor(thread) as executor:
-                tasks = [asyncio.create_task(extractor(url)) for url in urls_list]
-                for coro in asyncio.as_completed(tasks):
-                    result = await coro
-                    keywords.update(result)
+            tasks = [extractor(url) for url in urls_list]
+            results = await asyncio.gather(*tasks)
+            for result in results:
+                keywords.update(result)
 
     for keyword in keywords:
         print(keyword)
